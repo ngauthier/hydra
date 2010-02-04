495d86392d8cce1b7cf822233501553b95ee1e05
Nick Gauthier <nick@smartlogicsolutions.com>
2010-02-04 11:25:07 -0500

    documented hash

--- lib/hydra/hash.rb
+++ lib/hydra/hash.rb
@@ -1,10 +1,12 @@
 class Hash
+  # Stringify the keys in the hash. Returns a new hash.
   def stringify_keys
     inject({}) do |options, (key, value)|
       options[key.to_s] = value
       options
     end
   end
+  # Stringify the keys in the hash in place.
   def stringify_keys!
    keys.each do |key|
      self[key.to_s] = delete(key)
